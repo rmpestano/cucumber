2b7a64882bf25c3a20906ef394eb140e1fc3e9a9
lhamello <lhamello@gmail.com>
2015-01-21 10:38:13 -0200

    Adicionada classes para ordenação e paginação de pesquisas.

--- cucumber/src/main/java/lham/projects/cucumber/infra/AbstractEntity.java
+++ cucumber/src/main/java/lham/projects/cucumber/infra/AbstractEntity.java
@@ -2,6 +2,8 @@ package lham.projects.cucumber.infra;
 
 import java.io.Serializable;
 
+import javax.persistence.Transient;
+
 /**
  * Classe base para as classes de entidade.
  * 
@@ -12,20 +14,31 @@ import java.io.Serializable;
  */
 public abstract class AbstractEntity<K> implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * Retorna a chave primária da entidade.
-     * 
-     * @return a chave primária da entidade.
-     */
-    public abstract K getPrimaryKey();
+	/**
+	 * Retorna a chave primária da entidade.
+	 * 
+	 * @return a chave primária da entidade.
+	 */
+	public abstract K getPrimaryKey();
 
-    /**
-     * Define a chave primária da entidade.
-     * 
-     * @param primaryKey
-     *            chave primária.
-     */
-    public abstract void setPrimaryKey(final K primaryKey);
+	/**
+	 * Define a chave primária da entidade.
+	 * 
+	 * @param primaryKey
+	 *            chave primária.
+	 */
+	public abstract void setPrimaryKey(final K primaryKey);
+
+	@Transient
+	private PropLista propLista = new PropLista();
+
+	protected PropLista getPropLista() {
+		return propLista;
+	}
+
+	protected void setPropLista(PropLista propLista) {
+		this.propLista = propLista;
+	}
 }
---
+++ cucumber/src/main/java/lham/projects/cucumber/infra/PropLista.java
@@ -0,0 +1,53 @@
+package lham.projects.cucumber.infra;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PropLista implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int inicio;
+	private int tamanho;
+	private Map<String, Object> filtros;
+	private List<Ordem> ordem;
+
+	public PropLista(int inicio, int tamanho) {
+		super();
+		this.inicio = inicio;
+		this.tamanho = tamanho;
+	}
+
+	public PropLista() {
+		this.filtros = new HashMap<>();
+		this.ordem = new ArrayList<>();
+	}
+
+	public int getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(int inicio) {
+		this.inicio = inicio;
+	}
+
+	public int getTamanho() {
+		return tamanho;
+	}
+
+	public void setTamanho(int tamanho) {
+		this.tamanho = tamanho;
+	}
+
+	public Map<String, Object> getFiltros() {
+		return filtros;
+	}
+
+	public List<Ordem> getOrdem() {
+		return ordem;
+	}
+
+}
