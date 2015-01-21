2cee77aff6410e768f9a6542ac8894d38a3406a7
lhamello <lhamello@gmail.com>
2015-01-21 14:19:19 -0200

    Colocado todos IT na classe suite

--- cucumber/src/test/java/lham/projects/cucumber/test/SuiteTest.java
+++ cucumber/src/test/java/lham/projects/cucumber/test/SuiteTest.java
@@ -2,6 +2,7 @@ package lham.projects.cucumber.test;
 
 import lham.projects.cucumber.continente.ContinenteIT;
 import lham.projects.cucumber.infra.AppSettingsBeanUT;
+import lham.projects.cucumber.pais.PaisIT;
 import lham.projects.cucumber.regiao.RegiaoIT;
 
 import org.junit.runner.RunWith;
@@ -9,7 +10,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AppSettingsBeanUT.class, ContinenteIT.class, RegiaoIT.class })
+@SuiteClasses({ AppSettingsBeanUT.class, ContinenteIT.class, PaisIT.class, RegiaoIT.class })
 public class SuiteTest {
 
 }
